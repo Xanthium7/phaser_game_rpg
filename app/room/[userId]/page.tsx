0d6101c2158739edd6240807f8500bbafd7dc7d9
Xanthium7 <akshhaykmurali@gmail.com>
2024-11-19 23:36:00 +0530

    yea

--- app/room/[userId]/page.tsx
+++ app/room/[userId]/page.tsx
@@ -4,7 +4,7 @@ async function Page({ params }: { params: { userId: string } }) {
   const { userId } = await params;
   return (
     <div className="flex h-screen w-screen flex-col justify-center items-center  ">
-      <h1 className="font-semibold">THE GAME YEAH</h1>
+      <h1 className="font-semibold">Loading YEAH</h1>
       <h1 className="text-3xl">id: {userId}</h1>
 
       <Game />
