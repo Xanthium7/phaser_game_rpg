4a32d9fca9f2b5d410ca0d5b6cca55df2ebf7695
Xanthium7 <akshhaykmurali@gmail.com>
2024-11-26 12:35:33 +0530

    Add authentication pages and integrate Clerk for user management

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,32 @@
-import Game from "@/components/Game";
-import Image from "next/image";
+"use client";
+
+import { useAuth, useUser } from "@clerk/nextjs";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
+  const { isLoaded, isSignedIn, user } = useUser();
+  if (!isLoaded || !isSignedIn) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <SignedOut>
+          <div className="bg-gray-900 text-white py-2 px-4 rounded">
+            <SignInButton />
+          </div>
+        </SignedOut>
+      </div>
+    );
+  }
   return (
     <div className="flex h-screen w-screen gap-11 flex-col justify-center items-center  ">
       <h1 className="font-semibold">THE GAME YEAH</h1>
-      <h1 className="text-3xl">Do Authenticate My guy</h1>
-      <button className="bg-gray-900 text-white py-2 px-4 rounded">
-        AUTHENTICATE
-      </button>
 
-      <a href="/room">temporary redirect</a>
+      <div>Hello, {user.firstName} welcome to Clerk</div>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <a href="/room" className="bg-gray-800 text-white py-2 px-4 rounded">
+        Create A ROOM
+      </a>
     </div>
   );
 }
