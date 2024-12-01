b927941e0325f0c742a15200fefde0f0f7ca88f7
Xanthium7 <akshhaykmurali@gmail.com>
2024-12-01 21:16:57 +0530

    Got the player name working the issue was with Reac Strict mode cause double renders

--- app/room/[userId]/page.tsx
+++ app/room/[userId]/page.tsx
@@ -3,22 +3,25 @@ import Game from "@/components/Game";
 import { useParams } from "next/navigation";
 import Room from "@/components/Room";
 import { useUser } from "@clerk/nextjs";
+import { useState, useEffect } from "react";
 
 function Page() {
   const params = useParams<{ tag: string; item: string }>();
-  // const { isLoaded, isSignedIn, user } = useUser();
+  const { isLoaded, isSignedIn, user } = useUser();
 
-  // if (!isLoaded || !isSignedIn) {
-  //   return null;
-  // }
+  if (!isLoaded || !isSignedIn) {
+    return null;
+  }
+  console.log("Username:", user.username);
+  console.log("Type of user.username:", typeof user?.username);
 
   return (
     <div className="w-full flex-col h-full justify-center items-center flex overflow-hidden">
       {/* <Room userId={userId}></Room>; */}
-      <h1 className="font-semibold">Loading...</h1>
-      <h1 className="text-3xl z-10">id: {params.userId}</h1>
+
+      <h1 className="text-3xl z-10">Room id: {params.userId}</h1>
       {/* <div>Hello, {user.username}!</div> */}
-      <Game userId={params.userId}></Game>
+      <Game userId={params.userId} name={user.username}></Game>
     </div>
   );
 }
