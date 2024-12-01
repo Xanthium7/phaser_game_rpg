8dd35c3a2c7a338e3e6db19d05394b360b21f489
Xanthium7 <akshhaykmurali@gmail.com>
2024-12-01 19:26:32 +0530

    Refactor room page to use useParams for userId and remove Room component

--- app/room/[userId]/page.tsx
+++ app/room/[userId]/page.tsx
@@ -1,12 +1,24 @@
-// import Game from "@/components/Game";
+"use client";
+import Game from "@/components/Game";
+import { useParams } from "next/navigation";
 import Room from "@/components/Room";
+import { useUser } from "@clerk/nextjs";
 
-async function Page({ params }: { params: { userId: string } }) {
-  const { userId } = await params;
+function Page() {
+  const params = useParams<{ tag: string; item: string }>();
+  // const { isLoaded, isSignedIn, user } = useUser();
+
+  // if (!isLoaded || !isSignedIn) {
+  //   return null;
+  // }
 
   return (
-    <div className="w-full h-full justify-center items-center flex overflow-hidden">
-      <Room userId={userId}></Room>;
+    <div className="w-full flex-col h-full justify-center items-center flex overflow-hidden">
+      {/* <Room userId={userId}></Room>; */}
+      <h1 className="font-semibold">Loading...</h1>
+      <h1 className="text-3xl z-10">id: {params.userId}</h1>
+      {/* <div>Hello, {user.username}!</div> */}
+      <Game userId={params.userId}></Game>
     </div>
   );
 }
