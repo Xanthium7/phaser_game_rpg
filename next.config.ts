d58b3de3e69e4fda7b4a09ea5f03f179fc884285
Xanthium7 <akshhaykmurali@gmail.com>
2025-07-29 19:38:55 +0530

    refactor: remove image domain configuration from Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  /* config options here */
   reactStrictMode: false,
-  images: {
-    domains: ["i.pinimg.com"],
-  },
 };
 
 export default nextConfig;
