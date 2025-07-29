dfc67204114069cd2565c15792ea86eb90b17b04
Xanthium7 <akshhaykmurali@gmail.com>
2025-07-29 19:27:15 +0530

    refactor: remove mobile-specific layout code for cleaner structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import localFont from "next/font/local";
 
 import "./globals.css";
 import SmoothScrolling from "@/components/SmoothScrolling";
-import Image from "next/image";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -33,25 +32,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* Only show this on mobile, hide rest of content */}
-        <div className=" md:hidden fixed top-0 left-0 w-full h-full min-h-screen z-50 bg-black text-white flex flex-col items-center justify-center text-center">
-          <Image
-            src={
-              "https://i.pinimg.com/736x/9a/37/a3/9a37a37cc4881997a3786e61f0b25c47.jpg"
-            }
-            alt="Mobile View"
-            width={500}
-            height={500}
-            className=""
-          ></Image>
-          <span className="text-lg pt-10 font-bold">VIEW IN DESKTOP</span>
-        </div>
-        {/* Only show app content on desktop */}
-        <div className="hidden md:block">
-          <SmoothScrolling>
-            <ClerkProvider>{children}</ClerkProvider>
-          </SmoothScrolling>
-        </div>
+        <SmoothScrolling>
+          <ClerkProvider>{children}</ClerkProvider>
+        </SmoothScrolling>
       </body>
     </html>
   );
