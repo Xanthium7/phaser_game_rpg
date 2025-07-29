5718a7c52393f16ea7065b875d4ba7cd6dcb4d9a
Xanthium7 <akshhaykmurali@gmail.com>
2025-07-29 19:19:56 +0530

    feat: add responsive layout for mobile and desktop views with image prompt

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: false,
+  images: {
+    domains: ["i.pinimg.com"],
+  },
 };
 
 export default nextConfig;
